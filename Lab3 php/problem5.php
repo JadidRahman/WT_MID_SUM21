e2cb1c04c3b36a7200e0c4da279496e5e44b8991
JadidRahman <jadidrahman@outlook.com>
2021-06-13 13:30:15 +0600

    Lab-3 100% Done

--- Lab3 php/problem5.php
+++ Lab3 php/problem5.php
@@ -1,10 +1,34 @@
-<?php
-    $backColor= "yellow"; $fontColor="cyan"; $imageWidth="300px"; $imageHeight="300px";
-?>
+<?php 
+     $backcolor="yellow";
+     $fontcolor="cyan";
+     $imageWidth="300px";
+     $imageHeight="300px";
+ ?>
 
  
 
-    <img src="aiub_logo.png" width="<?php echo $imageWidth;?>" height="<?php echo $imageheight;?>" />
-    <h2>American International University -<span style="background-color:<?php echo $backColor;?>;color:<?php echo $fontColor;?>">
-    Bangladesh</h2>
-</span>
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8">
+     <title>Problem-5</title>
+ </head>
+ <body>
+       <table>
+                 <tr>
+                       <td>
+                             <img src="aiub_logo.png" height="<?php echo $imageHeight; ?>" width="<?php echo $imageWidth; ?>" >
+
+ 
+
+                       </td>
+
+ 
+
+                       <td>
+                             <span ><h2 style="background-color:<?php echo $backcolor;?>; color: <?php echo $fontcolor; ?>;">American International University-Bangladesh </h2></span>
+                       </td>
+                 </tr>
+       </table>
+ </body>
+ </html>
